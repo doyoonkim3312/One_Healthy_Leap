56cd6a479470b38a09e363856959f407d9fa66cd
Doyoon Kim <doyoon3312@kakao.com>
2021-11-10 21:21:44 -0500

    FEAT::[111021] Add basic codes for Bluetooth.
    * Info.plist has been refactored with bluetooth related configurations.
    * Viewcontroller.swift: Basic codes and functions for bluetooth communication have been added.

--- One-Healthy-Leap/One-Healthy-Leap/ViewController.swift
+++ One-Healthy-Leap/One-Healthy-Leap/ViewController.swift
@@ -2,18 +2,208 @@
 //  ViewController.swift
 //  One-Healthy-Leap
 //
-//  Created by 김도윤 on 11/3/21.
+//  Created by Doyoon Kim on 11/3/21.
 //
 
 import UIKit
+import CoreBluetooth
+import Foundation
+
+struct CBUUIDs{
+
+    static let kBLEService_UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e"
+    static let kBLE_Characteristic_uuid_Tx = "6e400002-b5a3-f393-e0a9-e50e24dcca9e"
+    static let kBLE_Characteristic_uuid_Rx = "6e400003-b5a3-f393-e0a9-e50e24dcca9e" // Identify bluetooth connection (device)
+
+    static let BLEService_UUID = CBUUID(string: kBLEService_UUID)
+    static let BLE_Characteristic_uuid_Tx = CBUUID(string: kBLE_Characteristic_uuid_Tx)//(Property = Write without response)
+    static let BLE_Characteristic_uuid_Rx = CBUUID(string: kBLE_Characteristic_uuid_Rx)// (Property = Read/Notify)
+
+}
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stepsAchievementLabel: UILabel!
+    
+    // Add CBCentralManager instance.
+    var centralManager: CBCentralManager!
+    
+    private var bluefruitPeripheral: CBPeripheral!
+    
+    private var txCharacteristic: CBCharacteristic!
+    private var rxCharacteristic: CBCharacteristic!
+    
+    //private var receivedData
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        centralManager = CBCentralManager(delegate: self, queue: nil)
     }
-
+    
+    // Example function that reads data over Bluetooth connection.
+    
+    func startScanning() -> Void {
+      // Start Scanning (Scanning Bluetooth Peripherals
+      centralManager?.scanForPeripherals(withServices: [CBUUIDs.BLEService_UUID])
+    }
+    
+    
+    
+    
+    
+    func disconnectFromDevice () {
+        if bluefruitPeripheral != nil {
+        centralManager?.cancelPeripheralConnection(bluefruitPeripheral!)
+        }
+     }
+    
+    
+    
+    // Write value to Peripheral
+    func writeOutgoingValue(data: String){
+          
+        let valueString = (data as NSString).data(using: String.Encoding.utf8.rawValue)
+        
+        if let bluefruitPeripheral = bluefruitPeripheral {
+              
+          if let txCharacteristic = txCharacteristic {
+                  
+            bluefruitPeripheral.writeValue(valueString!, for: txCharacteristic, type: CBCharacteristicWriteType.withResponse)
+              }
+          }
+      }
 
 }
+
+extension ViewController: CBCentralManagerDelegate {
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        
+         switch central.state {
+              case .poweredOff:
+                  print("Is Powered Off.")
+              case .poweredOn:
+                  print("Is Powered On.")
+                  startScanning()
+              case .unsupported:
+                  print("Is Unsupported.")
+              case .unauthorized:
+              print("Is Unauthorized.")
+              case .unknown:
+                  print("Unknown")
+              case .resetting:
+                  print("Resetting")
+              @unknown default:
+                print("Error")
+              }
+      }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral,advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        
+        
+        bluefruitPeripheral = peripheral
+        bluefruitPeripheral.delegate = self
+
+        print("Peripheral Discovered: \(peripheral)")
+        print("Peripheral name: \(peripheral.name)")
+        print ("Advertisement Data : \(advertisementData)")
+        
+        centralManager?.connect(bluefruitPeripheral!, options: nil)
+        
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+       bluefruitPeripheral.discoverServices([CBUUIDs.BLEService_UUID])
+    }
+    
+}
+
+extension ViewController: CBPeripheralManagerDelegate {
+
+  func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+    switch peripheral.state {
+    case .poweredOn:
+        print("Peripheral Is Powered On.")
+    case .unsupported:
+        print("Peripheral Is Unsupported.")
+    case .unauthorized:
+    print("Peripheral Is Unauthorized.")
+    case .unknown:
+        print("Peripheral Unknown")
+    case .resetting:
+        print("Peripheral Resetting")
+    case .poweredOff:
+      print("Peripheral Is Powered Off.")
+    @unknown default:
+      print("Error")
+    }
+  }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+            print("*******************************************************")
+
+            if ((error) != nil) {
+                print("Error discovering services: \(error!.localizedDescription)")
+                return
+            }
+            guard let services = peripheral.services else {
+                return
+            }
+            //We need to discover the all characteristic
+            for service in services {
+                peripheral.discoverCharacteristics(nil, for: service)
+            }
+            print("Discovered Services: \(services)")
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+           
+               guard let characteristics = service.characteristics else {
+              return
+          }
+
+          print("Found \(characteristics.count) characteristics.")
+
+          for characteristic in characteristics {
+
+            if characteristic.uuid.isEqual(CBUUIDs.BLE_Characteristic_uuid_Rx)  {
+
+              rxCharacteristic = characteristic
+
+              peripheral.setNotifyValue(true, for: rxCharacteristic!)
+              peripheral.readValue(for: characteristic)
+
+              print("RX Characteristic: \(rxCharacteristic.uuid)")
+            }
+
+            if characteristic.uuid.isEqual(CBUUIDs.BLE_Characteristic_uuid_Tx){
+              
+              txCharacteristic = characteristic
+              
+              print("TX Characteristic: \(txCharacteristic.uuid)")
+            }
+          }
+    }
+    
+    // READ DATA OVER THE BLUETOOTH.
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+
+          var characteristicASCIIValue = NSString()
+
+          guard characteristic == rxCharacteristic,
+
+          let characteristicValue = characteristic.value,
+          let ASCIIstring = NSString(data: characteristicValue, encoding: String.Encoding.utf8.rawValue) else { return }
+          // Line above is for reading the data over bluetooth and saving it as a string.
+        
+          characteristicASCIIValue = ASCIIstring
+        
+        let recievedValues = (characteristicASCIIValue as NSString).integerValue
+        //receivedData.append(recievedValues)
+
+          print("Value Recieved: \((characteristicASCIIValue as String))")
+    }
+}
+
 
